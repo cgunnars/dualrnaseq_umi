1bfd3fa5be6a7593b7f309186682b16cdf413154
cgunnars <gunnars@mit.edu>
2024-04-05 14:33:31 +0000

    add test startup script

---
+++ dual_startup.sh
@@ -0,0 +1,63 @@
+sudo apt-get install rpm
+sudo apt-get install pigz
+sudo apt-get install wget
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
+echo 'installing conda'
+
+#install bcl2fastq
+conda install -c dranew bcl2fastq
+chmod +x bcl2fastq.sh
+echo 'installing bcl2fastq'
+
+#install fastqc
+sudo apt-get -y install fastqc
+echo 'installing fastqc'
+
+#install umitools
+sudo apt-get -y install gcc
+pip install umi_tools
+echo 'installing umi_tools'
+
+#install rsem for host alignment
+conda install -c bioconda rsem
+
+# install nextflow
+export OS=linux ARCH=amd64
+export NXF_VER=21.10.0
+export NXF_MODE=google
+curl https://get.nextflow.io | bash
+
+#install git
+conda install git
+
+# install Singularity requirements
+sudo apt-get update && sudo apt-get install -y \
+    build-essential \
+    libssl-dev \
+    uuid-dev \
+    libgpgme11-dev \
+    squashfs-tools \
+    libseccomp-dev \
+    pkg-config
+
+# install GO
+export VERSION=1.19.3 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+# set up environment variables
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+    source ~/.bashrc
+curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | \
+    sh -s -- -b $(go env GOPATH)/bin v1.21.0
+git clone --recursive https://github.com/sylabs/singularity.git
+singularity version
+cd singularity
+sudo apt-get install glib2-devel
+sudo apt-get install libgtk2.0-dev
+./mconfig
+sudo make -C builddir install
+
+./nextflow run cgunnars/dualrnaseq_umi -profile test,singularity
